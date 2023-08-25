9ef5bbfd1afd08d8b8c00a43561564ea4fd0131f
Victor Cobuci <41239354+cobuci@users.noreply.github.com>
2023-08-25 01:42:31 -0300

    feat: Adição de cards , Navbar , MUI
    -Adicionado os cards para exibição dos dados resgatados.
    -Adição do MUI com MaterialUI
    -Search Box funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+
 import axios from "axios";
 import { Fragment, useEffect, useState } from "react";
+import ChampionCard from "./components/ChampionCard";
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import { AppBar, Box, Container } from "@mui/material";
+import Navbar from "./components/Navbar";
 
 
 function App() {
 
-  const [heroes, setHeroes] = useState([{}]);
+  const [heroes, setHeroes] = useState([{}])
 
   const getChamps = () => {
     axios
@@ -18,22 +23,42 @@ function App() {
 
   const mappedChampions = Object.keys(heroes).map((champion) => ({
     name: heroes[champion].name,
+    id: heroes[champion].id,
     tags: heroes[champion].tags,
+    description: heroes[champion].blurb,
   }));
+
+
+  const championFilter = (name) => {
+    const filteredChamp = []
+    if (name ===""){
+      getChamps()
+    }
+    for (let i in heroes) {
+      if (heroes[i].name.includes(name)) {
+        filteredChamp.push(heroes[i])
+      }
+    }
+    setHeroes(filteredChamp)
+  }
+
+  const showChampions = mappedChampions.map((champion, key) => {
+    return (
+      <Grid md={3} key={key}>
+        <ChampionCard name={champion.name} img={champion.id} description={champion.description} />
+      </Grid>
+    )
+  });
 
   return (
     <div>
-      {mappedChampions.map((champion , key) => {
-        return (
-          <Fragment key={key}>
-            <p>
-              {champion.name}
-            </p>
-            <p>  {champion.tags}
-            </p>   </Fragment>
-        )
+      <Navbar championFilter={championFilter} />
+      <Container maxWidth='xg'>
+        <Grid container spacing={2}>
+          {showChampions}
+        </Grid>
+      </Container>
 
-      })}
     </div>
   );
 
