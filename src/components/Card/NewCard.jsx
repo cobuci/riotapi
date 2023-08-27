d52c54b94cd57b792c32444504e13d82108cf444
Victor Cobuci <cobuci80@gmail.com>
2023-08-27 06:14:27 -0300

    First commit
    - Implementação da rotação dos cards

--- src/components/Card/NewCard.jsx
+++ src/components/Card/NewCard.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-
+import { useState } from 'react';
 import './card.css'
 
 
@@ -43,27 +43,43 @@ export default function NewCard({ name, description, img, keyChamp, tag }) {
         backgroundImage: `url(${getTagImageUrl(tag2)})`,
     };
 
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const handleDivClick = () => {
+        setIsFlipped(!isFlipped);
+    };
 
     return (
         <React.Fragment >
-            <div className='card' style={imgChamp}>
-                <div className="lateral">
-                    <div className="regiao"></div>
-                    <p className="champion"> {name} </p>
-                </div>
-                <div className="espaco">
-                <div className="footer">
-                    <div className="tagDiv">
-                        <div className="tag" style={imgTag}></div>
-                        <div className="tag" style={imgTag2}></div>
-                    </div>
-                    <p>{description}</p>
-                </div></div>
-                <div className="id">
-                    <p>#{keyChamp}</p>
-                </div>
 
+            <div className="flip-container">
+                <div className="flipper">
+                    <div className="front">
+                        <div className='card' style={imgChamp}>
+                            <div className="lateral">
+                                <div className="regiao"></div>
+                                <p className="champion"> {name} </p>
+                            </div>
+                            <div className="espaco">
+                                <div className="footer">
+                                    <div className="tagDiv">
+                                        <div className="tag" style={imgTag}></div>
+                                        <div className="tag" style={imgTag2}></div>
+                                    </div>
+                                    <p>{description}</p>
+                                </div></div>
+                            <div className="id">
+                                <p>#{keyChamp}</p>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="back">
+                        <div className='card' style={imgChamp}></div>
+                    </div>
+                </div>
             </div>
+
 
         </React.Fragment>
     );
